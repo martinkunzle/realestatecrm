7892be26a7adf8a55139834c6200f478ce7676d6
martinkunzle <martin.kunzle@yahoo.com>
2026-09-14 12:55:04 -0400

    Simplify signup form to email and password

--- app/login/auth-form.tsx
+++ app/login/auth-form.tsx
@@ -42,12 +42,6 @@ export default function AuthForm({
           : "Enter your account details."}
       </p>
       <form action={submit} className="auth-form">
-        {signup && (
-          <label>
-            Full name
-            <input name="name" autoComplete="name" required />
-          </label>
-        )}
         <label>
           Email
           <input name="email" type="email" autoComplete="email" required />
